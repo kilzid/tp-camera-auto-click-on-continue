8450b4e5d5f2b6491b0f8761a4a29749f5c1d512
doron kilzi <kilzid@gmail.com>
2019-10-09 15:35:47 +0300

    Update and rename code.js to index.js

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 javascript:
 const autoRefresh = () => {
-  if (isVideoOnPause()) {
-    counter++;
-    if (counter <= minRetries) {
-      console.log('kilzi: pause but lower then minRetries', counter, new Date().toTimeString());
-      return;
-    };
-    counter = 0;
-    window.clearInterval(ids.cancelInterval);
+  if (shouldClickOnContinue()) {
+    window.clearInterval(ids.cancelAutoRefresh);
     clickOnContinue();
     ids.cancelIsUp = window.setInterval(() => {
       counter++;
       console.log('kilzi: checking if up', counter, new Date().toTimeString());
-      if (!isVideoOnPause()) {
+      if (isVideoIsPlaying()) {
         console.log('kilzi: we are up!', new Date().toTimeString());
         cancelIsUp();
       };
@@ -26,11 +20,15 @@ const autoRefresh = () => {
 
 const cancelIsUp = () => {
   window.clearInterval(ids.cancelIsUp);
-  ids.cancelInterval = window.setInterval(autoRefresh, 1 * 1000);
+  ids.cancelAutoRefresh = window.setInterval(autoRefresh, 1 * 1000);
   counter = 0;
 };
 
-const isVideoOnPause = () => $("#JSVideo").get(0).paused;
+const shouldClickOnContinue = () => {
+  return $('#continue').is(":visible") || $('.network-tips').is(":visible");
+}
+
+const isVideoIsPlaying = () => !$("#JSVideo").get(0).paused;
 
 const clickOnContinue = () => {
   console.log("kilzi: click on continue at", new Date().toTimeString());
@@ -43,4 +41,4 @@ const minRetries = 5;
 const max_retries = 20;
 let counter = 0;
 const ids = {};
-ids.cancelInterval = window.setInterval(autoRefresh, 1 * 1000);
+ids.cancelAutoRefresh = window.setInterval(autoRefresh, 1 * 1000);
