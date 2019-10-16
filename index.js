75056793c3083952e788f1db27362a43264777f4
doron kilzi <doron.kilzi@oath.com>
2019-10-16 21:00:06 +0300

    code refactor

--- index.js
+++ index.js
@@ -1,44 +1,77 @@
 javascript:
-const autoRefresh = () => {
+console.log("https://www.minifier.org/");
+console.log('kilzi: starting at', new Date().toTimeString());
+
+cleanLastRun();
+init();
+window.ids.cancelAutoRefresh = window.setInterval(autoRefresh, 1 * 1000);
+
+function autoRefresh() {
   if (shouldClickOnContinue()) {
-    window.clearInterval(ids.cancelAutoRefresh);
+    window.clearInterval(window.ids.cancelAutoRefresh);
     clickOnContinue();
-    ids.cancelIsUp = window.setInterval(() => {
-      counter++;
-      console.log('kilzi: checking if up', counter, new Date().toTimeString());
+    window.ids.cancelIsUp = window.setInterval(() => {
+      window.counter++;
+      console.log('kilzi: checking if up', window.counter, new Date().toTimeString());
       if (isVideoIsPlaying()) {
         console.log('kilzi: we are up!', new Date().toTimeString());
         cancelIsUp();
       };
-      if (counter > max_retries) {
+      if (window.counter > window.max_retries) {
         console.warn('kilzi: still down. trying to refresh', new Date().toTimeString());
         cancelIsUp();
       };
     }, 1 * 1000);
+  } else if (isVideoIsPlaying()) {
+    window.lastPlaying = new Date().valueOf();
+  } else {
+    now = new Date().valueOf();
+    diff = now - window.lastPlaying;
+    if ((diff / 1000) > window.max_retries) {
+      window.lastPlaying = now;
+      clickOnContinue();
+    }
   }
 };
 
-const cancelIsUp = () => {
-  window.clearInterval(ids.cancelIsUp);
-  ids.cancelAutoRefresh = window.setInterval(autoRefresh, 1 * 1000);
-  counter = 0;
+function cancelIsUp() {
+  window.clearInterval(window.ids.cancelIsUp);
+  window.counter = 0;
+  window.lastPlaying = new Date().valueOf();
+  window.ids.cancelAutoRefresh = window.setInterval(autoRefresh, 1 * 1000);
 };
 
-const shouldClickOnContinue = () => {
-  return $('#continue').is(":visible") || $('.network-tips').is(":visible");
+function shouldClickOnContinue() {
+  if ($('.network-tips').is(":visible")) {
+    console.warn('kilzi: network error. trying to refresh', new Date().toTimeString());
+    return true;
+  };
+  if ($('#refresh').is(":visible")) {
+    console.warn('kilzi: camera is offline. trying to refresh', new Date().toTimeString());
+    return true;
+  };
+  return $('#continue').is(":visible");
 };
 
-const isVideoIsPlaying = () => !$("#JSVideo").get(0).paused;
+function isVideoIsPlaying() {
+  return !$("#JSVideo").get(0).paused;
+};
 
-const clickOnContinue = () => {
+function clickOnContinue () {
   console.log("kilzi: click on continue at", new Date().toTimeString());
   $('#continue').click();
 };
 
-console.log("https://www.minifier.org/");
-console.log('kilzi: starting at', new Date().toTimeString());
-const minRetries = 5;
-const max_retries = 20;
-let counter = 0;
-const ids = {};
-ids.cancelAutoRefresh = window.setInterval(autoRefresh, 1 * 1000);
+function cleanLastRun() {
+  if (window.ids) {
+    window.clearInterval(window.ids.cancelAutoRefresh);
+    window.clearInterval(window.ids.cancelIsUp);
+  }
+};
+
+function init() {
+  window.max_retries = 20;
+  window.counter = 0;
+  window.ids = {};
+  clickOnContinue();
+};
